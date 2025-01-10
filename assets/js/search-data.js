0bd8ce07f26758d91c79ee179d1aca1a7d4f5e0e
jkwd <31514956+jkwd@users.noreply.github.com>
2025-01-10 11:32:52 +0000

    Deploying to gh-pages from @ jkwd/jkwd.github.io@f6bd54acd4d708b357796d26207add84411ddddd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,18 +56,7 @@ ninja.data = [{
           window.open("https://towardsdatascience.com/profiling-my-favorite-songs-on-spotify-through-clustering-33fee591783d?source=rss-4b20231e95c9------2", "_blank");
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+    },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
